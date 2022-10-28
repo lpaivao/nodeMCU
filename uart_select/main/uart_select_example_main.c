0b24f3b357ff60b8c2ff5dc40e3a4208c86fc697
lpaivao <109324115+lpaivao@users.noreply.github.com>
2022-10-28 10:14:44 -0300

    Add files via upload

--- uart_select/main/uart_select_example_main.c
+++ uart_select/main/uart_select_example_main.c
@@ -36,89 +36,33 @@
 #define BAUD_RATE_STD 9600
 
 //Tamanho do buffer da uart de 256 bits
-#define BUF_SIZE 256
+#define BUF_SIZE 24
 
 /**
  * RX Function
 */
-void receiveData(){
+/*void receiveData(uint8_t *data){
 
     // Configure a temporary buffer for the incoming data
-    uint8_t *data = calloc(32 ,sizeof(data));
+    uint8_t *data = calloc(3 ,sizeof(data));
 
     uart_read_bytes(UART_NUM_0, data, BUF_SIZE, 20 / portTICK_RATE_MS);
     free(data);
-}
+} */
 
 /**
  * TX Function
 */
-void sendData(){
+/*void sendData(){
     // Configure a temporary buffer for the sending data
-    uint8_t *data = calloc(32 ,sizeof(data));
+    uint8_t *data = calloc(3 ,sizeof(data));
     size_t size = sizeof(data);
 
     uart_write_bytes(UART_NUM_0, (const char *) data, size);
     free(data);
-}
+} */
 
-void configGPIO(gpio_config_t *gpio_config_var, gpio_num_t GPIO_NUMBER, gpio_mode_t modeIO){
-    gpio_config_var->mode = modeIO;
-    gpio_config(&gpio_config_var);
-}
-
-void setup_GPIOs(){
-
-    gpio_config_t *gpio14;
-    configGPIO(gpio14, GPIO_NUM_14, GPIO_MODE_INPUT);   //D5
-
-    gpio_config_t *gpio12;
-    configGPIO(gpio12, GPIO_NUM_12, GPIO_MODE_INPUT);   //D6
-
-    gpio_config_t *gpio13;
-    configGPIO(gpio13, GPIO_NUM_13, GPIO_MODE_INPUT);   //D7
-
-    gpio_config_t *gpio15;
-    configGPIO(gpio15, GPIO_NUM_15, GPIO_MODE_INPUT);   //D8
-
-// -------------------------------------------------------------------
-
-    gpio_config_t *gpio3;
-    configGPIO(gpio3, GPIO_NUM_3, GPIO_MODE_INPUT);     //RX
-
-    gpio_config_t *gpio1;
-    configGPIO(gpio1, GPIO_NUM_1, GPIO_MODE_OUTPUT);    //TX
-
-
-    // 1. init adc (A0)
-    adc_config_t adc_config;   //A0
-
-    // Depend on menuconfig->Component config->PHY->vdd33_const value
-    // When measuring system voltage(ADC_READ_VDD_MODE), vdd33_const must be set to 255.
-    adc_config.mode = ADC_READ_TOUT_MODE;
-    adc_config.clk_div = 8; // ADC sample collection clock = 80MHz/clk_div = 10MHz
-    ESP_ERROR_CHECK(adc_init(&adc_config));
-}
-
-/**
- * Setup para UART0
- */
-
-void uart0_init(void) {
-  uart_config_t uart_config = {
-      .baud_rate = BAUD_RATE_STD,             // constante 9600 baud rate
-      .data_bits = UART_DATA_8_BITS,          // 8 bit bytes
-      .parity = UART_PARITY_DISABLE,          // no partity
-      .stop_bits = UART_STOP_BITS_1,          // one stop bit
-      .flow_ctrl = UART_HW_FLOWCTRL_DISABLE,  // no flow control
-      .rx_flow_ctrl_thresh = 0,               // unused
-  };
-  uart_param_config(UART_NUM_0, &uart_config);
-  uart_driver_install(UART_NUM_0, BUF_SIZE, BUF_SIZE, 0, NULL, 0);
-}
-
-
-bool protocolosRequisicao(uint8_t comando, uint_fast8_t enderecoSensor){
+/* bool protocolosRequisicao(uint8_t comando, uint_fast8_t enderecoSensor){
 
     //uint8_t offset = GPIO_REG_READ(enderecoSensor); //calcula PERIPHS_GPIO_BASEADDR + enderecoSensor
 
@@ -136,9 +80,9 @@ bool protocolosRequisicao(uint8_t comando, uint_fast8_t enderecoSensor){
     }
     else
         return false;
-}
+} */
 
-uint_fast8_t protocolosResposta(uint8_t resposta){
+/* uint_fast8_t protocolosResposta(uint8_t resposta){
 
     if(resposta == NODE_COM_PROBLEMA){
     }
@@ -167,9 +111,62 @@ void testeSensor(uint8_t address, gpio_config_t *gpio){
     else if(address == GPIO_PIN15_ADDRESS){    
         gpio_get_level(GPIO_NUM_15);
     }
+} */
+
+/**
+ * Setup para UART0
+ */
+
+void uart0_init(void) {
+  uart_config_t uart_config = {
+      .baud_rate = BAUD_RATE_STD,             // constante 9600 baud rate
+      .data_bits = UART_DATA_8_BITS,          // 8 bit bytes
+      .parity = UART_PARITY_DISABLE,          // no partity
+      .stop_bits = UART_STOP_BITS_1,          // one stop bit
+      .flow_ctrl = UART_HW_FLOWCTRL_DISABLE,  // no flow control
+      .rx_flow_ctrl_thresh = 0,               // unused
+  };
+  uart_param_config(UART_NUM_0, &uart_config);
+  uart_driver_install(UART_NUM_0, BUF_SIZE, BUF_SIZE, 0, NULL, 0);
+}
+
+void configGPIO(gpio_config_t io_conf, uint32_t pin_bit_mask, gpio_mode_t mode){
+    //disable interrupt
+    io_conf.intr_type = GPIO_INTR_DISABLE;
+    //set as output mode
+    io_conf.mode = mode;
+    //bit mask of the pins that you want to set
+    io_conf.pin_bit_mask = pin_bit_mask;
+    //disable pull-down mode
+    io_conf.pull_down_en = 0;
+    //disable pull-up mode
+    io_conf.pull_up_en = 0;
+    //configure GPIO with the given settings
+    gpio_config(&io_conf);
+    printf("GPIO configured succefully");
+
 }
 
 void app_main() {
 
-   
+    uart0_init();
+
+    gpio_config_t gpio14;
+    configGPIO(gpio14, GPIO_Pin_14, GPIO_MODE_OUTPUT);
+
+    // 1. init adc (A0)
+    adc_config_t adc_config;   //A0
+
+    // Depend on menuconfig->Component config->PHY->vdd33_const value
+    // When measuring system voltage(ADC_READ_VDD_MODE), vdd33_const must be set to 255.
+    adc_config.mode = ADC_READ_TOUT_MODE;
+    adc_config.clk_div = 8; // ADC sample collection clock = 80MHz/clk_div = 10MHz
+    ESP_ERROR_CHECK(adc_init(&adc_config));
+
+    printf("Driver is installed on UART0: ");
+    if(uart_is_driver_installed(UART_NUM_0) == true){
+        printf("True");
+    }
+    else
+        printf("False");
 }
